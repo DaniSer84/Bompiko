6fe1dda4d71fd0a8f3458cb2813b690c9c3cb9ec
DaniSer84 <serenelli84@gmail.com>
2024-04-28 10:33:11 +0200

    buttons' events improved

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let menuButton = document.querySelector('#hamburger')
 let headerLinks = document.querySelector('.header-links')
 let listaServizi = document.querySelector('.lista-servizi')
+let containers = Array.from(listaServizi.querySelectorAll('div'))
 
 listaServizi.addEventListener('click', (e) => {
     let pressedButton = e.target.closest('span')
@@ -12,55 +13,20 @@ listaServizi.addEventListener('click', (e) => {
     }
 )
 
-
-// let buttons = Array.from(listaServizi.querySelectorAll('span'))
-let containers = Array.from(listaServizi.querySelectorAll('div'))
-
-// let comunicazioneButton = document.querySelector('#comunicazione-btn')
-// let comunicazioneDiv = document.querySelector('#comunicazione-text')
-// let marketingButton = document.querySelector('#marketing-btn')
-// let marketingDiv = document.querySelector('#marketing-text')
-// let identityButton = document.querySelector('#identity-btn')
-// let identityDiv = document.querySelector('#identity-text')
-// let consulenzaButton = document.querySelector('#consulenza-btn')
-// let consulenzaDiv = document.querySelector('#consulenza-text')
-// let formazioneButton = document.querySelector('#formazione-btn')
-// let formazioneDiv = document.querySelector('#formazione-text')
-
-
 menuButton.addEventListener('click', () => {
-    menuButton.classList.toggle('closed')
-    headerLinks.classList.toggle('open')
+    toggleMenu(menuButton, headerLinks)
 })
 
 headerLinks.addEventListener('click', (e) => {
     if (e.target.tagName === 'A') {
-        menuButton.classList.toggle('closed')
-        headerLinks.classList.toggle('open')
+        toggleMenu(menuButton, headerLinks)
     }
 })
 
-
-// comunicazioneButton.addEventListener('click', () => {
-//     toggleText(comunicazioneButton, comunicazioneDiv)
-
-// })
-// marketingButton.addEventListener('click', () => {
-//     toggleText(marketingButton, marketingDiv)
-
-// })
-// identityButton.addEventListener('click', () => {
-//     toggleText(identityButton, identityDiv)
-
-// })
-// consulenzaButton.addEventListener('click', () => {
-//     toggleText(consulenzaButton, consulenzaDiv)
-
-// })
-// formazioneButton.addEventListener('click', () => {
-//     toggleText(formazioneButton, formazioneDiv)
-
-// })
+function toggleMenu(button, container) {
+    button.classList.toggle('closed')
+    container.classList.toggle('open')
+}
 
 function toggleText(button, container) {
     container.classList.toggle('hide')
