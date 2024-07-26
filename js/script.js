6f1f67fa0624ea6f41602f0c8683538e5e528def
DaniSer84 <serenelli84@gmail.com>
2024-07-26 15:18:18 +0200

    added back on top button

--- js/script.js
+++ js/script.js
@@ -36,3 +36,27 @@ function toggleText(button, container) {
             button.textContent = 'X'
         }
 }
+
+
+/// back on top
+let botBtn = document.getElementById('bot-btn')
+
+// the button appear when the user scrolls downs n px:
+let n = 30
+window.onscroll = () => {scrollFunction()}
+
+function scrollFunction() {
+    if (document.body.scrollTop > n || document.documentElement.scrollTop > n) {
+        botBtn.style.display = 'block'
+    } else {
+        botBtn.style.display = 'none'
+    }
+}
+
+// function to go back on top: 
+botBtn.addEventListener('click', topFunction)
+
+function topFunction() {
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0
+}
