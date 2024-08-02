9a96ffb58e1fa7318f0b0d6318d663f03da2703c
DaniSer84 <serenelli84@gmail.com>
2024-08-02 14:59:45 +0200

    updates fot creating index.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,9 @@
 import { scrollFunction, toggleMenu, topFunction } from "./modules/functions.js"
-import { createServicesList } from "./modules/listaServizi.js"
-import { SERVIZI } from "./modules/servizi.js"
-
 
 let menuButton = document.querySelector('#hamburger')
 let headerLinks = document.querySelector('.header-links')
-let listaServizi = document.querySelector('.lista-servizi')
 let botBtn = document.getElementById('bot-btn')
 
-createServicesList(SERVIZI, listaServizi)
 
 menuButton.addEventListener('click', () => {
     toggleMenu(menuButton, headerLinks)
